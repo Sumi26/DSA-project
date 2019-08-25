5f6203bf4e96c1b778f7716f7c3a4c5c1b112bb4
Sumi26 <46630571+Sumi26@users.noreply.github.com>
2019-08-25 21:07:52 +0530

    Display info.php

---
+++ Display info.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<?php
+define('DB_SERVER', 'localhost');
+	define('DB_USERNAME', 'root');
+	define('DB_PASSWORD', '');
+	define('DB_DATABASE', 'project');
+	$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+	session_start();
+	$user=$_SESSION["id"];
+	$sql = "SELECT id,name,class,roll,section FROM profile WHERE id = '$user'";
+	$res = mysqli_query($db,$sql);
+	$row = mysqli_fetch_array($res,MYSQLI_ASSOC);
+	$name=$row['name'];
+	$class=$row['class'];
+	$section=$row['section'];
+	$roll=$row['roll'];
+?>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Display info</title>
+	<!-- <script src="https://code.jquery.com/jquery-3.3.1.js" -->
+			  <!-- integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" -->
+			  <!-- crossorigin="anonymous"></script> -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" >
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Arvo:400,400i" rel="stylesheet">
+
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-light" class="navbar navbar-light" style="background-color: #e3f2fe;">
+  <a class="navbar-brand" href="index_new.php"><img alt="Logo-img" src="logo.PNG" width="50px" height="70px"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Profile
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="Display info.php">Display info</a>
+
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Academics
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+          <a class="dropdown-item" href="Timetable.php">Timetable</a>
+          <a class="dropdown-item" href="Study_materials.php">Study Materials</a>
+          <a class="dropdown-item" href="Attendance.php">Attendance</a>
+          <a class="dropdown-item" href="Extracurricular_indexpage.php">Extra curricular</a>
+          <a class="dropdown-item" href="Calender.php">Calendar</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Examination
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="Examination_schedule.php">Schedule</a>
+          <a class="dropdown-item" href="Examination_result.php">Result</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Transport
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="Transport_registration.php">Registration</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Online Payment
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="Online payment.php">Choose payment</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Library
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="borrowed_books.php">Borrowed Books</a>
+          <a class="dropdown-item" href="Amount_due.php">Amount Due</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Hostel
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="mess_menu.php">Mess menu</a>
+        </div>
+      </li>
+    </ul>
+    <a href="Logout.php"><button type="button" class="btn btn-primary" style="background-color: #ffffff; color: #1111ff;">Sign Out</button></a>
+  </div>
+</nav>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<script>
+    $(document).ready(function () {
+        $('.dropdown-toggle').dropdown();
+    });
+</script>
+<h1 style="text-align: center; color: #1111aa;">Display information</h1>
+<div style="text-align:center; padding:50px; ">
+<h3 style="text-align:left;color:#0066ff;">  Name:<div id='page' style='width: 500px'>
+<?php
+echo "<h5 style='border:2px aliceblue solid; font-size:21px;'>".$name."</h5>";
+?>
+</div>
+</h3>
+<h3 style="text-align:left;color:#0066ff;"> Roll number:<div id='page' style='width: 500px'>
+<?php
+echo "<h5 style='border:2px aliceblue solid; font-size:21px;'>$roll</h5>";
+?>
+</div>
+</h3>
+<h3 style="text-align:left;color:#0066ff;">  Class:<div id='page' style='width: 500px'>
+<?php
+echo "<h5 style='border:2px aliceblue solid; font-size:21px;'>$class</h5>";
+?>
+</div>
+</h3>
+<h3 style="text-align:left;color:#0066ff;">  Section:<div id='page' style='width: 500px'>
+<?php
+echo "<h5 style='border:2px aliceblue solid; font-size:21px;'>$section</h5>";
+?>
+</div>
+</h3>
+</div>
+</body>
+</html>
