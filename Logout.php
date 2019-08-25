31143af8f6fb42ef6b914021c23885bfb87995b2
Sumi26 <46630571+Sumi26@users.noreply.github.com>
2019-08-25 21:17:54 +0530

    Logout.php

---
+++ Logout.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<?php
+session_start();
+session_destroy();
+?>
+<html lang="en">
+<link rel="stylesheet" type="text/css" href="Login_page.css" />
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Logout</title>
+	<!-- <script src="https://code.jquery.com/jquery-3.3.1.js" -->
+			  <!-- integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" -->
+			  <!-- crossorigin="anonymous"></script> -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" >
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Arvo:400,400i" rel="stylesheet">
+
+</head>
+<body>
+<br>
+<br>
+<h3>
+<div class="box blue" style="color: #1211ff" font size="10">
+<center style="padding: 50;">Successfully logged out</center>
+</div>
+</h3>
+<br>
+<br>
+<center><a href="Login_page.php" style="text-decoration: none;"><button type=submit style="background-color:#1111aa; color:#ffffff;">Go back to Login page</button></a></center>
+</script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
