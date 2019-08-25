b178559896b600ff8b8150b9fbd3424ecd179099
Sumi26 <46630571+Sumi26@users.noreply.github.com>
2019-08-25 21:06:21 +0530

    bankPage.php

---
+++ bankPage.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<?php
+session_start();
+?>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Bank</title>
+	<!-- <script src="https://code.jquery.com/jquery-3.3.1.js" -->
+			  <!-- integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" -->
+			  <!-- crossorigin="anonymous"></script> -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" >
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Arvo:400,400i" rel="stylesheet">
+
+</head>
+<body>
+	<center>
+	<div style="padding-top:100px; font-size:2vw; color: #122299; background-color: aliceblue; height: 500px; margin-top: 100px;">
+		----This webpage will be connected to the bank----
+	</div>
+	</center>
+</body>
+</html>
